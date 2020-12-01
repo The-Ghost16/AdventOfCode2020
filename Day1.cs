e68a3dc94240b7115c3c693b967628c9617e0cea
unknown <theghost16@gmail.com>
2020-12-01 11:07:31 +0100

    New project

---
+++ Day1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+
+namespace AdventOfCode2020 
+{
+    public class Day1
+    {
+        public void Assignment1() 
+        {
+            Console.WriteLine("Start with executing assignment 1!");
+
+            using(var sr = new StreamReader("inputs\\day1.txt"))
+            {
+
+            }
+
+            Console.WriteLine("Finished with executing assignment 1!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the problem solving solutions of Advent of Code 2020!");
+
+            Console.WriteLine("We're going to start with day 1!");
+            var day1 = new Day1();
+            day1.Assignment1();
+
+            Console.WriteLine("Finished all the assignments!");
+            Console.ReadLine();
+        }
+    }
+}
